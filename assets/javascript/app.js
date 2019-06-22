4fee7e87994e590dbffb99a5db1f7087079e46b4
jcasas5253 <skedabble@gmail.com>
2019-06-22 13:18:51 -0700

    music

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+function toggleMusic() {
+    var x = document.getElementById("content-container");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  console.log("created by Jacob Casas");
